61fa17387f0edcb433ac02ca8f6215e8130930c7
Vishalatis <vishal.atpuae@gmail.com>
2024-12-31 10:19:12 +0530

    first commit

---
+++ assets/templates/basic/css/color.php
@@ -0,0 +1,120 @@
+
+.nav-tabs .nav-link:hover, .nav-tabs .nav-link.active, .btn--base.active, .btn--base.active:focus, .btn--base.active:hover, .btn--base:focus, .btn--base:hover, .header-bottom-area .navbar-collapse .main-menu li a:hover, .header-bottom-area .navbar-collapse .main-menu li a.active, .navbar-toggler span, .banner-section .call-area a, .inner-banner-section .banner-content .title, .breadcrumb-item a, .breadcrumb-item.active::before, .footer-links li:hover, .footer-links li:hover::before, .order-item:hover .order-content .title, .order-content .ratings i, .food-item:hover .food-content .title, .food-content .title span, .restaurants-details-area .restaurants-details-content .ratings i, .restaurant-details-wrapper a, .restaurants-header-area .food-details-tab li a:hover, .edit-btn, .account-header .title, .profile-thumb-content .profile-content .user-info-list li i, .dashboard-icon, .blog-item:hover .blog-content .title a, .category-content li:hover, .cookie__wrapper .title, .cookie__wrapper .btn--close, .payment-item .payment-content .p .modal-content .modal-title, #infoModal .modal-header button {
+  color: #ff5722;
+}
+
+.text--base, .custom-btn, .restaurant-details-wrapper .btn--base {
+  color: #ff5722 !important;
+}
+
+.scrollToTop, .pagination .page-item.active .page-link, .pagination .page-item:hover .page-link, .radio-item [type="radio"]:checked + label:after, .radio-item [type="radio"]:not(:checked) + label:after, .item-discount, .item-discount::before, .item-discount::after, .account-header .title::before {
+  background: #ff5722;
+}
+
+*::-webkit-scrollbar-button, *::-webkit-scrollbar-thumb, .swiper-pagination .swiper-pagination-bullet-active, .slider-next, .slider-prev, .btn--base, .input-group-text, .custom-check-group input:checked + label::before, .btn--base, ::selection, .footer-toggle .right-icon, .subscribe-form button, .subscribe-form input[type="button"], .subscribe-form input[type="reset"], .subscribe-form input[type="submit"], .footer-social li a:hover, .footer-social li a.active, .order-thumb .offer-badge, .food-thumb .offer-badge, .food-thumb .cart-badge, .side-sidebar-close-btn, .food-cart-slider .food-wrapper .cart-badge, .checkout-details-header .title::before, .draw-countdown .syotimer__body .syotimer-cell, .account-btn-area .account-btn, .dash-btn, .modal-content .close, .modal-content .close, .contact-info-icon i {
+  background-color: #ff5722;
+}
+
+.bg--base {
+    background-color: #ff5722 !important;
+}
+
+.pagination .page-item.active .page-link, .pagination .page-item:hover .page-link {
+  border-color: #ff5722;
+}
+
+.btn--base.active:focus, .btn--base.active:hover, .btn--base:focus, .btn--base:hover, .cookie__wrapper .read-policy, .btn--base.active {
+  border: 1px solid #ff5722;
+}
+
+.nav-tabs .nav-link.active, .section-header .section-title, .restaurants-header-area .food-details-tab li a.active, .checkout-widget-title {
+  border-bottom: 2px solid #ff5722;
+}
+
+.header-bottom-area .navbar-collapse .main-menu li .sub-menu {
+  border-top: 2px solid #ff5722;
+}
+
+.footer-area {
+  background-color: #ff57220a;
+}
+.subscribe-form input{
+  border: 1px solid #ff57227a;
+}
+.offer-section {
+  border-bottom: 3px solid #ff5722;
+}
+
+.dashboard-item {
+  border-left: 4px solid #ff5722;
+}
+
+.cookie__wrapper {
+  border-top: 1px solid #ff5722;
+}
+
+.footer-social li a{
+  border : 1px solid #ff5722;
+}
+.footer-social li a i{
+  color : #ff5722;
+}
+
+.copy-text a{
+  color: #ff5722;
+}
+
+.copyright-wrapper {
+  border-top: 1px solid #ff57223b;
+}
+
+.dropdown-list>.dropdown-list__item:hover {
+  background-color: #ff5722!important;
+}
+
+
+.form-group .form-check-input:checked{
+  background-color: #ff5722;
+  border-color:  #ff5722;
+}
+.form-group .form-check-input{
+  border: 1px solid  #ff5722;
+}
+
+.form-group a{
+  color : #ff5722;
+}
+
+.select2-container--default .select2-search--dropdown .select2-search__field {
+  border-color: 1px solid #ff5722 !important;
+}
+
+.link{
+  color: #ff5722;
+}
+
+:root{
+  --main : #ff5722;
+}
+
+
+.verification-code span {
+  border: 1px solid #ff572244 !important;
+  color: #ff572244 !important;
+}
+
+.link:hover{
+  color: #ff5722;
+}
+
+@media only screen and (max-width: 991px) {
+  .custom-table tbody tr td::before {
+    color: #ff5722;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .restaurant-details-wrapper a{
+    background-color: #ff5722;
+  }
+}
